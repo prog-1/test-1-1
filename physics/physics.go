d578331a8f56095741c9d7c89112d577c9534ba3
Marija Patrīcija Žunda <mpzunda@edu.riga.lv>
2021-10-11 10:04:16 +0300

    editing physics

---
+++ physics/physics.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The program calculate and print the total capacity of the condensers connected in series.")
+	fmt.Println("Enter capacity of three condensers: C1, C2, C3:")
+	var C1, C2, C3 float64
+	fmt.Scanln(&C1, &C2, &C2)
+	
+
+}
