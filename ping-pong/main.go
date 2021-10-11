b1b2e91299580afe92a805dec217e756b5662a82
unknown <valerijamax@gmail.com>
2021-10-11 10:49:07 +0300

    just wanted to correct ping-pong

--- ping-pong/main.go
+++ ping-pong/main.go
@@ -6,15 +6,18 @@ import (
 
 func main() {
 	fmt.Println("The program prints Ping, Pong or a number.")
-	var a float64
+	var a int
 	fmt.Println("Enter a number:")
 	fmt.Scan(&a)
 	if a%2 == 0 {
-		fmt.Println("Ping")
+		if a%5 == 0 {
+			fmt.Println("PingPong")
+		} else {
+			fmt.Println("Ping")
+		}
 	} else if a%5 == 0 {
 		fmt.Println("Pong")
-	} else if a%5 == 0 && a%2 == 0 {
-		fmt.Println("PingPong")
+	} else {
+		fmt.Println(a)
 	}
-	fmt.Println(a)
 }
